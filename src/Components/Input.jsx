d463465118a9d4dbc05624abd4f0103c8b82df3e
Natnael Tesfaye <natnael.tesfaye2022@gmail.com>
2025-06-05 13:10:27 +0200

    filtering tasks

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -6,23 +6,23 @@ const Input = ({ task, handleChange, handleClick, editIndex }) => {
     };
   
     return (
-      <>
+      <> 
         <form onSubmit={handleSubmit}>
           <div className="input-container">
-            <input
+            <input 
               type="text"
               name="title"
               placeholder="Title"
               value={task.title}
               onChange={handleChange}
+              required
             />
             <textarea
               name="detail"
               rows="10"
               placeholder="Details"
               value={task.detail}
-              onChange={handleChange}
-              required
+              onChange={handleChange} 
             ></textarea>
           <button type="submit">{editIndex !== null ? "Update" : "Add"}</button>
           </div>
